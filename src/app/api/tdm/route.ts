a814e2c9061320088a1af11dd673750515fb85dd
Mohammadreza <mohammadreza.ahadiyan@gmail.com>
2025-03-12 12:33:39 +0330

    api test changes

--- src/app/api/tdm/route.ts
+++ src/app/api/tdm/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: NextRequest) {
   } catch (err) {
     console.log(err);
     return NextResponse.json(
-      { message: "Database connection failed" },
+      { message: err },
       { status: 500 }
     );
   }
