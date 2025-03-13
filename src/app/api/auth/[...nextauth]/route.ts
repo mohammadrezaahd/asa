d62f10b1c458bb15b334a3c790e96a1e3ff0676d
Mohammadreza <mohammadreza.ahadiyan@gmail.com>
2025-03-13 13:56:58 +0330

    Add auth user to db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,27 @@
 import { authOptions } from "@/app/lib/auth";
 import NextAuth from "next-auth/next";
+import connectToDb from "@/server/database/connection";
+import getTable from "@/server/database/tables";
 
-const handler = NextAuth(authOptions);
+const userModel = getTable("User");
+
+const handler = NextAuth({
+  ...authOptions,
+  callbacks: {
+    async signIn({ user }) {
+      await connectToDb();
+      const existingUser = await userModel.findOne({ email: user.email });
+      if (!existingUser) {
+        await userModel.create({
+          name: user.name,
+          email: user.email,
+          image: user.image,
+          role: "user",
+        });
+      }
+      return true;
+    },
+  },
+});
 
 export { handler as GET, handler as POST };
