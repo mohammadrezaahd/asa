3ddd326f9105af85076e97edbb974af6d5840b1f
Mohammadreza <mohammadreza.ahadiyan@gmail.com>
2025-08-27 21:05:42 +0330

    change auth helper folder

--- src/app/api/auth/status/route.ts
+++ src/app/api/auth/status/route.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/lib/auth";
+import { authOptions } from "@/helpers/auth";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
